1a4bab35dfdfa86ed03830adb6c346b37590baf6
Sakib Hossain <Sakibhossain.nstu@gmail.com>
2016-02-26 00:26:52 +0600

    Enter Your English Date And Get Bangla Date.
    Idea was adapted from Codemask Online Contest

---
+++ Bangla Calender.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char bang[12][10]={"Poush", "Magh", "Falgun", "Chaitra","Baishakh", "Jaishtha", "Ashar", "Sraban", "Bhadra", "Ashwin", "Kartik" ,"Agrahayan"};
+int ff[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+int rr[12]={30,30,30,30,31,31,31,31,31,30,30,30};
+int lip(int y)
+{
+    if(y%100==0)
+    {
+        if(y%400==0)
+            return 1;
+        else
+            return 0;
+    }
+    else
+    {
+        if(y%4==0)
+            return 1;
+        else
+            return 0;
+    }
+}
+void d(int dd,int mm,int yy,int c)
+{
+    int de=18,me=1,i,j;
+    for(i=1;i<=12;i++)
+    {
+        for(j=1;j<=ff[i-1];j++)
+        {if(dd==j && mm==i)
+                break;
+            de++;
+            if(de>rr[me-1])
+            {
+                de=1;
+                me++;
+                if(me>12)
+                    me=1;
+            }
+
+        }
+        if(dd==j && mm==i)
+                break;
+    }
+if(dd==14 && mm==4 || dd>=14 && mm==4 || mm>4)
+    yy++;
+    cout<<"The Bangla Date is-\n"<<de<<", "<<bang[me-1]<<", "<<yy<<endl<<endl;
+}
+int main()
+{
+int test,i,j,k,dd,mm,yy,lp;
+cout<<"Enter Your Desire Test Case\n";
+cin>>test;
+for(i=1;i<=test;i++)
+{
+cout<<"Enter The English Day(day/month/year):\n";
+    cin>>dd>>mm>>yy;
+    if(lip(yy) && mm==3 && dd==14 )
+        cout<<"Case "<<i<<": 31, Falgun, "<<yy-594<<endl;
+    d(dd,mm,yy-594,i);
+}
+return 0;
+}
+
